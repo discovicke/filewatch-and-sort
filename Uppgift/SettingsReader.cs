f9187e2c6f247f3cb7da7e2ece66ae9197b7fd3b
discovicke <discovicke@gmail.com>
2025-10-24 15:48:35 +0200

    WIP SettingsReader reads root-elements

--- Uppgift/SettingsReader.cs
+++ Uppgift/SettingsReader.cs
@@ -1,6 +1,37 @@
+using System.Xml.Linq;
+using Uppgift.Models;
+
 namespace Uppgift;
 
-public class SettingsReader
+public static class SettingsReader
 {
-    
+    private const string ConfigFileName = "Inställningar.xml";
+
+    public static Settings Load()
+    {
+        XDocument doc = XDocument.Load(ConfigFileName);
+        XElement settingsEl = doc.Element("Settings")!;
+
+        var settings = new Settings
+        {
+            LogPath = settingsEl.Element("Log")?.Value ?? string.Empty
+        };
+
+        foreach (var dirEl in settingsEl.Elements("Directory"))
+        {
+            var dir = new DirectorySetting
+            {
+                Name = dirEl.Element("Name")?.Value ?? string.Empty,
+                Input = dirEl.Element("Input")?.Value ?? string.Empty,
+                Output = dirEl.Element("Output")?.Value ?? string.Empty,
+                Types = dirEl.Elements("Type").Select(t => t.Value.ToLower()).ToList()
+            };
+
+            settings.Directories.Add(dir);
+        }
+
+        Logger.LogPath = settings.LogPath;
+
+        return settings;
+    }
 }
