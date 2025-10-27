758171e424ab96f3bd3dfd6dbe9cd1607437a877
discovicke <discovicke@gmail.com>
2025-10-27 11:26:08 +0100

    WIP File Move

--- Uppgift/Program.cs
+++ Uppgift/Program.cs
@@ -1,14 +1,28 @@
-﻿using Uppgift;
+﻿using Uppgift.Models;
+using Uppgift;
 
 public class Program
 {
     public static int Main()
     {
         if (!ConfigValidator.IsValidConfigFile("Inställningar.xml"))
-            return -1;
-        
-        
+            return 1;
 
+        Settings settings = SettingsReader.Load();
+
+        foreach (var dir in settings.Directories)
+        {
+            Thread.Sleep(1000);
+            var watcher = new FileSystemWatcher(dir.Input);
+            var mover = new FileMover();
+
+            watcher.EnableRaisingEvents = true;
+            watcher.Created += (sender, e) => 
+                mover.HandleFiles(e.FullPath, dir);
+
+            Console.WriteLine($"Bevakar {dir.Name}: {dir.Input}");
+        }
+        
         return 0;
     }
 }
